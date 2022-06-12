f2e24a78982bef5bc907db03239b374d441b3168
Xyveran <ESOrtiz@student.fullsail.edu>
2022-06-11 20:41:02 -0400

    Built and Positioned Calculator Buttons

--- Lab1.5 CalculatorDesignUI/CalculatorWindow.cpp
+++ Lab1.5 CalculatorDesignUI/CalculatorWindow.cpp
@@ -1,33 +1,34 @@
 #include "CalculatorWindow.h"
 
-CalculatorWindow::CalculatorWindow() : wxFrame(nullptr, wxID_ANY, "Ortiz - Calculator", wxPoint(400, 200), wxSize(600, 600))
+CalculatorWindow::CalculatorWindow() : wxFrame(nullptr, wxID_ANY, "Ortiz - Calculator", wxPoint(50, 50), wxSize(310, 330))
 {
-	PlusButton = new wxButton(this, wxID_ANY, "+", wxPoint(390,190), wxSize(25, 25));
-	MinusButton = new wxButton(this, wxID_ANY, "-", wxPoint(380, 180), wxSize(25, 25));
+	PlusButton = new wxButton(this, wxID_ANY, "+", wxPoint(255,130), wxSize(25, 25));
+	MinusButton = new wxButton(this, wxID_ANY, "-", wxPoint(255, 160), wxSize(25, 25));
 	
-	DiviButton = new wxButton(this, wxID_ANY, "/", wxPoint(370, 170), wxSize(25, 25));
-	MultiButton = new wxButton(this, wxID_ANY, "*", wxPoint(360, 160), wxSize(25, 25));
+	DiviButton = new wxButton(this, wxID_ANY, "/", wxPoint(255, 190), wxSize(25, 25));
+	MultiButton = new wxButton(this, wxID_ANY, "*", wxPoint(255, 220), wxSize(25, 25));
 	
-	ModButton = new wxButton(this, wxID_ANY, "%", wxPoint(350, 150), wxSize(25, 25));
-	CButton = new wxButton(this, wxID_ANY, "c", wxPoint(390, 190), wxSize(25, 25));
+	ModButton = new wxButton(this, wxID_ANY, "%", wxPoint(225, 130), wxSize(25, 25));
+	CButton = new wxButton(this, wxID_ANY, "c", wxPoint(135, 130), wxSize(25, 25));
 	
-	EqualButton = new wxButton(this, wxID_ANY, "=", wxPoint(380, 180), wxSize(25, 25));
-	NegativeButton = new wxButton(this, wxID_ANY, "(-)", wxPoint(370, 170), wxSize(25, 25));
+	EqualButton = new wxButton(this, wxID_ANY, "=", wxPoint(255, 250), wxSize(25, 25));
+	NegativeButton = new wxButton(this, wxID_ANY, "(-)", wxPoint(165, 130), wxSize(25, 25));
 	
-	ZeroButton = new wxButton(this, wxID_ANY, "0", wxPoint(360, 160), wxSize(25, 25));
-	OneButton = new wxButton(this, wxID_ANY, "1", wxPoint(360, 160), wxSize(25, 25));
-	TwoButton = new wxButton(this, wxID_ANY, "2", wxPoint(350, 150), wxSize(25, 25));
-	ThreeButton = new wxButton(this, wxID_ANY, "3", wxPoint(340, 140), wxSize(25, 25));
-	FourButton = new wxButton(this, wxID_ANY, "4", wxPoint(340, 140), wxSize(25, 25));
+	ZeroButton = new wxButton(this, wxID_ANY, "0", wxPoint(165, 250), wxSize(55, 25));
+	OneButton = new wxButton(this, wxID_ANY, "1", wxPoint(165, 220), wxSize(25, 25));
+	TwoButton = new wxButton(this, wxID_ANY, "2", wxPoint(195, 220), wxSize(25, 25));
+	ThreeButton = new wxButton(this, wxID_ANY, "3", wxPoint(225, 220), wxSize(25, 25));
+	FourButton = new wxButton(this, wxID_ANY, "4", wxPoint(165, 190), wxSize(25, 25));
 	
-	FiveButton = new wxButton(this, wxID_ANY, "5", wxPoint(390, 190), wxSize(25, 25));
-	SixButton = new wxButton(this, wxID_ANY, "6", wxPoint(380, 180), wxSize(25, 25));
-	SevenButton = new wxButton(this, wxID_ANY, "7", wxPoint(370, 170), wxSize(25, 25));
-	EightButton = new wxButton(this, wxID_ANY, "8", wxPoint(360, 160), wxSize(25, 25));
-	NineButton = new wxButton(this, wxID_ANY, "9", wxPoint(350, 150), wxSize(25, 25));
+	FiveButton = new wxButton(this, wxID_ANY, "5", wxPoint(195, 190), wxSize(25, 25));
+	SixButton = new wxButton(this, wxID_ANY, "6", wxPoint(225, 190), wxSize(25, 25));
+	SevenButton = new wxButton(this, wxID_ANY, "7", wxPoint(165, 160), wxSize(25, 25));
+	EightButton = new wxButton(this, wxID_ANY, "8", wxPoint(195, 160), wxSize(25, 25));
+	NineButton = new wxButton(this, wxID_ANY, "9", wxPoint(225, 160), wxSize(25, 25));
 	
 	
-	BinaryButton = new wxButton(this, wxID_ANY, "Bin", wxPoint(340, 140), wxSize(25, 25));
-	HexButton = new wxButton(this, wxID_ANY, "Hex", wxPoint(340, 140), wxSize(25, 25));
-	DeciButton = new wxButton(this, wxID_ANY, "Dec", wxPoint(340, 140), wxSize(25, 25));
+	BinaryButton = new wxButton(this, wxID_ANY, "Bin", wxPoint(135, 220), wxSize(25, 55));
+	HexButton = new wxButton(this, wxID_ANY, "Hex", wxPoint(135, 160), wxSize(25, 55));
+	DeciButton = new wxButton(this, wxID_ANY, ".", wxPoint(225, 250), wxSize(25, 25));
 }
+		
